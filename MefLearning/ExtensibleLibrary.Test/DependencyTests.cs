8e2f94dc82e03c171c682293691bca242f7d6f22
Ty Smith <dts5004@gmail.com>
2017-08-16 21:07:17 -0700

    Added basic dependency injection tests

---
+++ MefLearning/ExtensibleLibrary.Test/DependencyTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensibleLibrary.Test
+{
+    [TestClass]
+    public class DependencyTests
+    {
+        public CompositionContainer CompositionContainer { get; set; }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            AggregateCatalog aggregateCatalog = new AggregateCatalog();
+            aggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(Car).Assembly));
+            CompositionContainer = new CompositionContainer(aggregateCatalog);
+        }
+
+        [TestMethod]
+        public void Dependencies_Can_Be_Imported()
+        {
+            // arrange
+            Car car = CompositionContainer.GetExportedValue<Car>();
+
+            // act
+            ;
+
+            // assert
+            Assert.IsNotNull(car.BodyType);
+            Assert.IsNotNull(car.TransmissionType);
+        }
+
+        [TestMethod]
+        public void GetExportedValue_Requires_Export_Attribute()
+        {
+            Assert.ThrowsException<ImportCardinalityMismatchException>(
+                () => CompositionContainer.GetExportedValue<NonExportedCar>());
+        }
+    }
+}
---
+++ MefLearning/ExtensibleLibrary/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensibleLibrary
+{
+    [Export]
+    public class Car
+    {
+        [Import]
+        public BodyType BodyType { get; set; }
+
+        [Import]
+        public TransmissionType TransmissionType { get; set; }
+    }
+     
+    public class NonExportedCar
+    {
+        [Import]
+        public BodyType BodyType { get; set; }
+
+        [Import]
+        public TransmissionType TransmissionType { get; set; }
+    }
+
+    public class BodyType
+    {
+        
+    }
+
+    [Export(typeof(BodyType))]
+    public class FourDoor : BodyType
+    {
+        
+    }
+
+    public class TransmissionType
+    {
+        
+    }
+
+    [Export(typeof(TransmissionType))]
+    public class Automatic : TransmissionType
+    {
+
+    }
+}
